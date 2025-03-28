3e8a5df18883149d3a952755a9ac2b2b12a9acbd
parker-nelson1 <121211783+parker-nelson1@users.noreply.github.com>
2025-03-28 14:10:47 -0700

    grub reinstall for Arch distro
    mount boot and efi
    
    install linux 
    
    install grub
    
    update grub cfg
    
    reboot

---
+++ grub.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Mount the root partition
+mount /dev/sda2 /mnt
+
+# Mount the EFI partition
+mount /dev/sda1 /mnt/boot/efi
+
+# Change root into the mounted system
+arch-chroot /mnt <<EOF
+# Install linux kernel
+pacman -S linux -y
+
+# Install GRUB bootloader for EFI systems
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+
+# Generate GRUB configuration file
+grub-mkconfig -o /boot/grub/grub.cfg
+EOF
+
+# Exit the chroot environment
+exit
+
+# Reboot the system
+reboot
